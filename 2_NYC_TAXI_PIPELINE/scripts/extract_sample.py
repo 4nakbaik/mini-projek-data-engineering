a99083d1edf20eb671b8ab288efca421c1e403ea
4nakbaik <shoumadono@gmail.com>
2026-02-26 21:23:56 +0700

    NYC TAXI Pipeline

--- 2_NYC_TAXI_PIPELINE/scripts/extract_sample.py
+++ 2_NYC_TAXI_PIPELINE/scripts/extract_sample.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+input_path = "../data/raw/yellow_tripdata_2015-01.csv"
+output_path = "../data/processed/sample_1.csv"
 
-input_path = os.path.join(BASE_DIR, "data/raw/yellow_tripdata_2015-01.csv")
-output_path = os.path.join(BASE_DIR, "data/processed/sample_1.csv")
+if not os.path.exists(input_path):
+    raise FileNotFoundError("Raw dataset not found.")
 
-df = pd.read_csv(input_path, nrows=1000)
-df.to_csv(output_path, index=False)
+chunk_iter = pd.read_csv(input_path, chunksize=100000)
+
+first_chunk = next(chunk_iter)
+sample = first_chunk.head(1000)
+
+sample.to_csv(output_path, index=False)
 
 print("Sample extracted successfully.")
--- 2_NYC_TAXI_PIPELINE/scripts/prepare.sh
+++ 2_NYC_TAXI_PIPELINE/scripts/prepare.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-RAW_DIR="../data/raw/yellow_tripdata_2015-01.csv"
-PROCESSED_DIR="../data/processed/sample_1.csv"
 LOG_FILE="../logs/prepare.log"
 
-echo "Pipeline started : $(date)" >> $LOG_FILE
+echo "Pipeline started: $(date)" >> $LOG_FILE
 
 python3 extract_sample.py >> $LOG_FILE 2>&1
 
-echo "Pipeline finished : $(date)" >> $LOG_FILE
+if [ $? -ne 0 ]; then
+    echo "Pipeline failed at $(date)" >> $LOG_FILE
+    exit 1
+fi
+
+echo "Pipeline finished successfully at $(date)" >> $LOG_FILE
